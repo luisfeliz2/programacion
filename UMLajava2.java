b92670a5da4311516d727ff3160e456570fe3e9b
luisfeliz2 <luisfelizbeltre@gmail.com>
2023-03-23 12:59:06 +0100

    cc

---
+++ UMLajava2.java
@@ -0,0 +1,25 @@
+ class Persona {
+    private String nombre;
+    private int edad;
+
+    public String mostrar(String nombre,int edad){...}
+
+}
+
+
+class Empleado extends Persona{
+    private int sueldo_bruto;
+    private List<Directivo> directores;
+    private Empresa empresa;
+    public String mostrar(){...}
+
+    public String mostrar(){...}
+    public double calcular_salario_nete
+}
+
+class Cliente extends Persona{
+    private String telefono_de_contacto;
+    public Cliente (String nombre,int edad){...}
+    public String mostrar(){...}
+
+}
